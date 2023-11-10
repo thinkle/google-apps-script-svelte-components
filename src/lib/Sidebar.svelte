37c535a4a7ab26cfa40bbac9b48588e18f3aa07a
Tom Hinkle <tmhinkle@gmail.com>
2023-11-10 12:33:10 -0500

    Calculate height of branding area and make content scroll over it

--- src/lib/Sidebar.svelte
+++ src/lib/Sidebar.svelte
@@ -1,12 +1,21 @@
-<script>
+<script lang="ts">
   import GoogleStyles from "./GoogleStyles.svelte";
+  let branding: HTMLDivElement;
+  let brandingHeight = 60;
+
+  function fixBrandingHeight(el: HTMLDivElement) {
+    brandingHeight = el.clientHeight;
+  }
 </script>
 
 <GoogleStyles />
-<div class="sidebar branding-below">
+<div
+  class="sidebar branding-below"
+  style:--branding-height="{brandingHeight}px"
+>
   <slot />
 </div>
-<div class="sidebar branding bottom">
+<div class="sidebar branding bottom" bind:this={branding} use:fixBrandingHeight>
   <slot name="branding">
     <span class="gray">
       Created with
@@ -21,3 +30,9 @@
     </span>
   </slot>
 </div>
+
+<style>
+  .branding-below {
+    max-height: calc(100% - var(--branding-height));
+  }
+</style>
